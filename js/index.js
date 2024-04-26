f86c625b3131d1804662f30c4c232ee4183570b4
amperefz <amperefz@gmail.com>
2024-04-26 19:05:45 -0300

    Avances sobre el diseño responsivo, programación del menú hamburguesa provisorio, algunas correcciones de estilo

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+/*Ver cómo resolver esto cuando veamos JS, entender los fundamentos*/
 document.addEventListener('DOMContentLoaded', function() {
     const iconos = document.querySelector('.iconos');
     const listadoNavbar = document.querySelector('.listadoNavbar');
